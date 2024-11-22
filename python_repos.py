6c1703e2f219257010d39a27a95752e517130055
AYu1130 <2963799545@qq.com>
2024-11-22 14:59:04 +0800

    教材17.1.6　概述最受欢迎的仓库

--- python_repos.py
+++ python_repos.py
@@ -13,21 +13,14 @@ response_dict = r.json()
 print(f"Total repositories: {response_dict['total_count']}")
 print(f"Complete results: {not response_dict['incomplete_results']}")
 
-# 探索有关仓库的信息
+# 研究有关仓库的信息
 repo_dicts = response_dict['items']
 print(f"Repositories retuened: {len(repo_dicts)}")
 
-# 研究第一个仓库
-repo_dict = repo_dicts[0]
-# print(f"\nKeys: {len(repo_dict)}")
-# for key in sorted(repo_dict.keys()):
-#     print(key)
-
 print("\nSelected information about first repository:")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}")
