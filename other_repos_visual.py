75a8d7f63a0f0a2fd0a71736c3dc9a40870c73a9
AYu1130 <2963799545@qq.com>
2024-11-29 15:20:45 +0800

    练习17.4  定制了图表的样式包括背景的透明度，字体颜色样式，边框线条的颜色粗细等，提取了项目仓库创建日期和最近更新日期，并在悬停文本中显示

--- other_repos_visual.py
+++ other_repos_visual.py
@@ -1,5 +1,6 @@
 import requests
 import plotly.express as px
+from datetime import datetime
 
 # 定义要查询的语言列表
 languages = ["JavaScript", "Ruby", "C", "Java", "Perl", "Haskell", "Go"]
@@ -27,11 +28,27 @@ for language in languages:
         stars = repo_dict['stargazers_count']
         owner = repo_dict['owner']['login']
         description = repo_dict['description']
-        hover_text = f"{owner}<br />{description}"
+        created_at = repo_dict['created_at']
+        updated_at = repo_dict['updated_at']
+        
+        # 格式化日期
+        created_date = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d")
+        updated_date = datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d")
+        
+        # 创建包含所有者、描述、创建日期和更新日期的悬浮文本
+        hover_text = (
+            f"{owner}<br />{description}<br />"
+            f"Created: {created_date}<br />"
+            f"Last Updated: {updated_date}"
+        )
+        
+        
         
         top_repos_dict[language].append({
             'repo_link': repo_link,
             'stars': stars,
+            'created_at': created_date,
+            'updated_at': updated_date,
             'hover_text': hover_text,
             'language': language
         })
@@ -42,6 +59,8 @@ all_repos = [repo for repos in top_repos_dict.values() for repo in repos]
 # 提取数据
 repo_links = [repo['repo_link'] for repo in all_repos]
 stars = [repo['stars'] for repo in all_repos]
+created_dates = [repo['created_at'] for repo in all_repos]
+updated_dates = [repo['updated_at'] for repo in all_repos]
 hover_texts = [repo['hover_text'] for repo in all_repos]
 languages = [repo['language'] for repo in all_repos]
 
@@ -51,9 +70,24 @@ labels = {'x': 'Repository', 'y': 'Stars', 'color': 'Language'}
 fig = px.bar(x=repo_links, y=stars, color=languages, title=title, labels=labels,
              hover_name=hover_texts)
 
-fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
-                  yaxis_title_font_size=20, xaxis_tickangle=-45)
+# 定制样式
+fig.update_layout(
+    title_font_size=28,
+    xaxis_title_font_size=20,
+    yaxis_title_font_size=20,
+    xaxis_tickangle=-45,
+    plot_bgcolor='rgba(0,0,0,0)',  # 设置背景透明
+    paper_bgcolor='rgba(0,0,0,0)',  # 设置绘图区域背景透明
+    legend_title_font_size=18,
+    font_family="Courier New",  # 设置字体
+    font_color="black",  # 设置字体颜色
+    yaxis=dict(gridcolor='lightgrey')  # 设置网格线颜色
+)
 
-fig.update_traces(marker_opacity=0.6)
+fig.update_traces(
+    marker_opacity=0.8,  # 设置条形图透明度
+    marker_line_width=2,  # 设置条形图边框宽度
+    marker_line_color='black'  # 设置条形图边框颜色
+)
 
 fig.show()
